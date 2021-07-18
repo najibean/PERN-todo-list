9b445e8844904dcf6d6a0dc7f6751783e655d210
Najibullah <Fuad.Najibullah@aia.com>
2021-07-18 20:01:52 +0800

    add database and docker-compose

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+// middleware
+app.use(cors())
+app.use(express.json()) // --> allows us to access the req.body
+
+
+app.listen(5000, () => {
+    console.log('Server is starting on port 5000')
+})
